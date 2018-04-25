7c4aabe4f5e7940414cff7e0d516014d7ba68006
trunglive <trunglive@gmail.com>
2018-04-25 18:00:43 -0400

    style(sidebar): add styling for sidebar

--- src/components/navigation/NavigationInfo.js
+++ src/components/navigation/NavigationInfo.js
@@ -4,8 +4,13 @@ export default class NavigationInfo extends Component {
   render() {
     return (
       <div className="nav-info">
-        Here is the Navigation Info
-        <p>position</p>
+        <img className="arrow-up" src="/icons/arrow-up.svg" />
+        <div className="section-index">
+          <span className="section-index--start">01</span>
+          <span className="section-index--splash">/</span>
+          <span className="section-index--end">03</span>
+        </div>
+        <img className="arrow-down" src="/icons/arrow-up.svg" />
       </div>
     );
   }
--- src/components/navigation/Sidebar.js
+++ src/components/navigation/Sidebar.js
@@ -4,8 +4,9 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
-        Side Bar
-        <p>position</p>
+        <p className="sidebar__logo">P.</p>
+        <img className="sidebar__setting-icon" src="/icons/setting.svg" />
+        <img className="sidebar__question-icon" src="icons/question.svg" />
       </div>
     );
   }
