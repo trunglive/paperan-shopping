e5572ce09854bd498ccbc0d3055b0015aa24e043
trunglive <trunglive@gmail.com>
2018-04-25 21:39:18 -0400

    style(nav-info): add styling & fixed position for nav-info slider

--- src/components/navigation/NavigationInfo.js
+++ src/components/navigation/NavigationInfo.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class NavigationInfo extends Component {
   render() {
     return (
       <div className="nav-info">
-        <img className="arrow-up" src="/icons/arrow-up.svg" />
-        <div className="section-index">
-          <span className="section-index--start">01</span>
-          <span className="section-index--splash">/</span>
-          <span className="section-index--end">03</span>
+        <div className="nav-info-main">
+          <img className="arrow-up" src="/icons/arrow-up.svg" />
+          <div className="section-index">
+            <span className="section-index--start">01</span>
+            <span className="section-index--splash">/</span>
+            <span className="section-index--end">03</span>
+          </div>
+          <img className="arrow-down" src="/icons/arrow-up.svg" />
         </div>
-        <img className="arrow-down" src="/icons/arrow-up.svg" />
       </div>
     );
   }
