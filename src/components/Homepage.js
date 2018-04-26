b40a71cdbd66ffc1c3ed326c25e1eb6aab9b6285
trunglive <trunglive@gmail.com>
2018-04-25 23:49:35 -0400

    feat(view): add new arrivals section

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -13,15 +13,80 @@ export default class HomePage extends Component {
             Discover collection
           </a>
         </div>
-        <div className="homepage-photo-grid">
-          <img src="/photos/homepage/notebook-0.jpg" alt="homepage photo 0" />
-          <img src="/photos/homepage/notebook-1.jpg" alt="homepage photo 1" />
-          <img src="/photos/homepage/notebook-2.jpg" alt="homepage photo 2" />
-          <img src="/photos/homepage/notebook-3.jpg" alt="homepage photo 3" />
-          <img src="/photos/homepage/notebook-4.jpg" alt="homepage photo 4" />
-          <img src="/photos/homepage/notebook-5.jpg" alt="homepage photo 5" />
-          <img src="/photos/homepage/notebook-6.jpg" alt="homepage photo 6" />
-          <img src="/photos/homepage/notebook-7.jpg" alt="homepage photo 7" />
+        <div className="homepage-featured">
+          <img
+            src="/photos/homepage/featured/notebook-ft-1.jpg"
+            alt="homepage featured photo 1"
+          />
+          <img
+            src="/photos/homepage/featured/notebook-ft-2.jpg"
+            alt="homepage featured photo 2"
+          />
+          <img
+            src="/photos/homepage/featured/notebook-ft-3.jpg"
+            alt="homepage featured photo 3"
+          />
+          <img
+            src="/photos/homepage/featured/notebook-ft-4.jpg"
+            alt="homepage featured photo 4"
+          />
+          <img
+            src="/photos/homepage/featured/notebook-ft-5.jpg"
+            alt="homepage featured photo 5"
+          />
+          <img
+            src="/photos/homepage/featured/notebook-ft-6.jpg"
+            alt="homepage featured photo 6"
+          />
+          <img
+            src="/photos/homepage/featured/notebook-ft-7.jpg"
+            alt="homepage featured photo 7"
+          />
+          <img
+            src="/photos/homepage/featured/notebook-ft-8.jpg"
+            alt="homepage featured photo 8"
+          />
+        </div>
+        <div className="homepage-new-arrivals">
+          <p>New Arrivals</p>
+          <div className="homepage-new-arrivals__grid">
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-1.jpg"
+              alt="homepage new arrivals photo 1"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-2.jpg"
+              alt="homepage new arrivals photo 2"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-3.jpg"
+              alt="homepage new arrivals photo 3"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-4.jpg"
+              alt="homepage new arrivals photo 4"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-5.jpg"
+              alt="homepage new arrivals photo 5"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-6.jpg"
+              alt="homepage new arrivals photo 6"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-7.jpg"
+              alt="homepage new arrivals photo 7"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-8.jpg"
+              alt="homepage new arrivals photo 8"
+            />
+            <img
+              src="/photos/homepage/new-arrivals/notebook-na-9.jpg"
+              alt="homepage new arrivals photo 9"
+            />
+          </div>
         </div>
       </div>
     );
