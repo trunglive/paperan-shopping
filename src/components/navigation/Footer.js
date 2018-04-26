3fc5c67efd4000c864d55f908db2a60eb7346a1e
trunglive <trunglive@gmail.com>
2018-04-25 22:39:07 -0400

    style(footer): add styling for footer

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        Here is the footer
-        <p>position</p>
+        <div className="footer__info">
+          <p>Info</p>
+          <ul>
+            <li>About Paperan</li>
+            <li>Blog</li>
+            <li>Press</li>
+            <li>FAQ</li>
+            <li>Giftcards</li>
+          </ul>
+        </div>
+        <div className="footer__contacts">
+          <p>Contacts</p>
+          <ul>
+            <li>Contact Us</li>
+            <li>Store Locations</li>
+          </ul>
+        </div>
+        <div className="footer__legal">
+          <p>Legal</p>
+          <ul>
+            <li>Terms & Conditions</li>
+            <li>Shopping & Delivery</li>
+            <li>Returns & Exchanges</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer__social item">
+          <p>Social</p>
+          <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Youtube</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
       </div>
     );
   }
