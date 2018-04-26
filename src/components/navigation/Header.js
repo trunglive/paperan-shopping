779afcc5345ba8c8bea665e24345347df1b0b49a
trunglive <trunglive@gmail.com>
2018-04-25 21:01:14 -0400

    style(search): style the search bar

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        Here is the header
-        <p>position</p>
+        <div className="search-bar-container">
+          <img
+            className="search-bar-container__search-icon"
+            src="/icons/search.svg"
+          />
+          <span>Search</span>
+        </div>
+        <div className="user-account-container">
+        <span>My account</span>
+        <img
+            className="user-account-container__cart-icon"
+            src="/icons/cart.svg"
+          />
+        </div>
       </div>
     );
   }
