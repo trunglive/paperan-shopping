2dc2e97061c8df37271bd9c4605a562b30acd2a2
trunglive <trunglive@gmail.com>
2018-05-05 18:51:48 -0400

    feat(redux-persist): persist items in cart on page refresh

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 const app = (
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
 );
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,28 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import rootReducer from "../reducers";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import rootReducer from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const configureStore = () => {
   const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeEnhancers(applyMiddleware(thunk, logger))
   );
-  return store;
+
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 };
 
 export default configureStore;
