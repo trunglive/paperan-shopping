0d0184f1702daa09a0ccbfbe46269d7de8fa3bd1
trunglive <trunglive@gmail.com>
2018-05-05 23:25:47 -0400

    feat(zoom-in): add magnification when hovering each product

--- src/components/single-item-page/photo-section/PhotoView.js
+++ src/components/single-item-page/photo-section/PhotoView.js
@@ -1,8 +1,28 @@
 import React from "react";
+import PaperanImageMagnify from "react-image-magnify";
+import ZoomHint from "./ZoomHint";
 
 const PhotoView = ({ photo_url, fullItemName }) => (
   <div className="photo-display">
-    <img src={photo_url} alt={fullItemName} />
+    <PaperanImageMagnify
+      {...{
+        smallImage: {
+          alt: fullItemName,
+          src: photo_url,
+          isFluidWidth: true
+        },
+        largeImage: {
+          src: photo_url,
+          width: 2000,
+          height: 1500
+        },
+        imageClassName: "photo-display__small",
+        enlargedImageClassName: "photo-display__large",
+        isHintEnabled: true,
+        shouldHideHintAfterFirstActivation: false,
+        hintComponent: () => <ZoomHint />,
+      }}
+    />
   </div>
 );
 
---
+++ src/components/single-item-page/photo-section/ZoomHint.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ZoomHint = () => (
+  <div className="zoom-hint">
+    <img className="zoom-in-icon" src="/icons/zoom-in.svg" alt="zoom in icon" />
+  </div>
+);
+
+export default ZoomHint;
