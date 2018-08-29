9a6a999a902e001457ddfbcc35ffc5629356adf6
trunglive <trunglive@gmail.com>
2018-08-29 19:51:55 -0400

    style(view): add css grid to checkout sections

--- src/components/checkout-page/CheckoutPage.js
+++ src/components/checkout-page/CheckoutPage.js
@@ -17,20 +17,30 @@ class CheckoutPage extends Component {
               <span>01</span>
               <span>Shipping</span>
             </div>
-            <div className="shipping-section__dhl-parcel-service" />
-            <div className="shipping-section__fedex-shipping" />
+            <div className="shipping-section__all-shipping-carriers checkout-page__grid">
+              <div className="shipping-section__dhl-parcel-service">
+                <input type="radio" name="shipping" />
+                <label>DHL Parcel Service</label>
+              </div>
+              <div className="shipping-section__fedex-shipping">
+                <input type="radio" name="shipping" />
+                <label>Fedex Shipping</label>
+              </div>
+            </div>
           </div>
           <div className="address-section">
             <div className="address-section__title checkout-page__title">
               <span>02</span>
               <span>Address</span>
             </div>
+            <div className="checkout-page__grid"></div>
           </div>
           <div className="payment-section">
             <div className="payment-section__title checkout-page__title">
               <span>03</span>
               <span>Payment</span>
             </div>
+            <div className="checkout-page__grid"></div>
           </div>
         </form>
         <div className="checkout-page__right-section" />
